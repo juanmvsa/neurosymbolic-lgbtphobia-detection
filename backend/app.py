6ecdae928efc75b0a6f83d4ed17f4ff28be5d997
Julian Bouchard <julianbouchard1@gmail.com>
2023-11-05 17:37:46 -0700

    Flask server now serves frontend

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, Response, request
 # define app
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='',
+            static_folder='../frontend/build',
+            template_folder='../frontend/build')
 
 
 @app.route('/')
 def index():
-    return '<h1>hello world</h1>'
-    # return render_template('index.html')
+    return render_template("index.html")
